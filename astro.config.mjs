eb5a9a8b6a22d6205438d26a8b65665696f85b69
Gabriel Ramos <luisgabrielramosrobles@gmail.com>
2026-06-02 17:13:08 -0400

    config: read base path from BASE_PATH env variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/folleto',
+  base: process.env.BASE_PATH || '/',
   integrations: [react()],
 
   vite: {
